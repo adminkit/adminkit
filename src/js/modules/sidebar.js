9de101fea726a273d087a4ed3c9eee190b0b2783
Paul Laros <paul@laros.io>
2020-09-14 17:06:23 +0200

    Fix console error on auth page #6

--- src/js/modules/sidebar.js
+++ src/js/modules/sidebar.js
@@ -2,17 +2,22 @@
 import SimpleBar from "simplebar";
 
 document.addEventListener("DOMContentLoaded", () => {
-  /* Initialize simplebar */
-  new SimpleBar(document.getElementsByClassName("js-simplebar")[0])
-
-  const sidebarElement = document.getElementsByClassName("sidebar")[0];
-  const sidebarToggleElement = document.getElementsByClassName("sidebar-toggle")[0];
-
-  sidebarToggleElement.addEventListener("click", () => {
-    sidebarElement.classList.toggle("collapsed");
-
-    sidebarElement.addEventListener("transitionend", () => {
-      window.dispatchEvent(new Event("resize"));
+  const simpleBarElement = document.getElementsByClassName("js-simplebar")[0];
+  
+  if(simpleBarElement){
+    /* Initialize simplebar */
+    new SimpleBar(document.getElementsByClassName("js-simplebar")[0])
+  
+    const sidebarElement = document.getElementsByClassName("sidebar")[0];
+    const sidebarToggleElement = document.getElementsByClassName("sidebar-toggle")[0];
+  
+    sidebarToggleElement.addEventListener("click", () => {
+      sidebarElement.classList.toggle("collapsed");
+  
+      sidebarElement.addEventListener("transitionend", () => {
+        window.dispatchEvent(new Event("resize"));
+      });
     });
-  });
+  }
+
 });
