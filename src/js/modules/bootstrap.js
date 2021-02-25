c5821fda026740668f1c1179450abb64dd0ad14c
Paul Laros <paul@laros.io>
2021-02-25 20:14:22 +0100

    expose bootstrap to window object

--- src/js/modules/bootstrap.js
+++ src/js/modules/bootstrap.js
@@ -1,15 +1,19 @@
-import { Tooltip, Popover } from "bootstrap";
+import * as bootstrap from "bootstrap";
 
 // Popovers
 // Note: Disable this if you're not using Bootstrap's Popovers
-const popoverTriggerList = [].slice.call(document.querySelectorAll("[data-toggle=\"popover\"]"))
+const popoverTriggerList = [].slice.call(document.querySelectorAll("[data-bs-toggle=\"popover\"]"))
 popoverTriggerList.map((popoverTriggerEl) => {
-  return new Popover(popoverTriggerEl)
+  return new bootstrap.Popover(popoverTriggerEl)
 })
 
 // Tooltips
 // Note: Disable this if you're not using Bootstrap's Tooltips
-const tooltipTriggerList = [].slice.call(document.querySelectorAll("[data-toggle=\"tooltip\"]"))
+const tooltipTriggerList = [].slice.call(document.querySelectorAll("[data-bs-toggle=\"tooltip\"]"))
 tooltipTriggerList.map((tooltipTriggerEl) => {
-  return new Tooltip(tooltipTriggerEl)
+  return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+// Bootstrap
+// Note: If you want to make bootstrap globally available, e.g. for using `bootstrap.modal`
+window.bootstrap = bootstrap;
