fb28d1005634207e04f2eb2ea56808ae6b6f14c1
Paul Laros <paul@laros.io>
2021-05-12 13:56:44 +0200

    update scss variables

--- src/js/modules/theme.js
+++ src/js/modules/theme.js
@@ -4,12 +4,23 @@
  */
 
 const theme = {
-  primary: "#3B7DDD",
-  secondary: "#6c757d",
-  success: "#28a745",
-  info: "#17a2b8",
-  warning: "#ffc107",
-  danger: "#dc3545"
+  "primary": "#3B7DDD",
+  "secondary": "#6c757d",
+  "success": "#1cbb8c",
+  "info": "#17a2b8",
+  "warning": "#fcb92c",
+  "danger": "#dc3545",
+  "white": "#fff",
+  "gray-100": "#f8f9fa",
+  "gray-200": "#e9ecef",
+  "gray-300": "#dee2e6",
+  "gray-400": "#ced4da",
+  "gray-500": "#adb5bd",
+  "gray-600": "#6c757d",
+  "gray-700": "#495057",
+  "gray-800": "#343a40",
+  "gray-900": "#212529",
+  "black": "#000"
 };
 
 // Add theme to the window object
