a47e08b808214c65cf387f1db83044f43d4ea58b
Paul Laros <paul@laros.io>
2021-05-12 13:54:35 +0200

    set font family for chartjs

--- src/js/modules/chartjs.js
+++ src/js/modules/chartjs.js
@@ -1,4 +1,5 @@
 // Usage: https://www.chartjs.org/
 import Chart from "chart.js";
 
+Chart.defaults.global.defaultFontFamily = "'Inter', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
 window.Chart = Chart;
