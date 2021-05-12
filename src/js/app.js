0e70e2e14560751448ebd31bd8eb9ca8210e3580
Paul Laros <paul@laros.io>
2021-05-12 13:55:53 +0200

    re-order app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,16 @@
 import "../scss/app.scss";
 
-// Global
+// AdminKit (required)
 import "./modules/bootstrap";
+import "./modules/sidebar";
 import "./modules/theme";
 import "./modules/feather";
-import "./modules/moment";
-import "./modules/sidebar";
-
-// Forms
-import "./modules/flatpickr";
 
 // Charts
 import "./modules/chartjs";
+
+// Forms
+import "./modules/flatpickr";
 
 // Maps
 import "./modules/vector-maps";
