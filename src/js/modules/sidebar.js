d7caaac9b62e8dfc8e421e2479228ee64f8dc6f4
Paul Laros <paul@laros.io>
2021-05-17 16:16:28 +0200

    Fix sidebar console error on auth pages

--- src/js/modules/sidebar.js
+++ src/js/modules/sidebar.js
@@ -7,32 +7,38 @@ const initialize = () => {
 }
 
 const initializeSimplebar = () => {
-  const simplebarInstance = new SimpleBar(document.getElementsByClassName("js-simplebar")[0]);
+  const simplebarElement = document.getElementsByClassName("js-simplebar")[0];
 
-  /* Recalculate simplebar on sidebar dropdown toggle */
-  const sidebarDropdowns = document.querySelectorAll(".js-sidebar [data-bs-parent]");
-  
-  sidebarDropdowns.forEach(link => {
-    link.addEventListener("shown.bs.collapse", () => {
-      simplebarInstance.recalculate();
+  if(simplebarElement){
+    const simplebarInstance = new SimpleBar(document.getElementsByClassName("js-simplebar")[0]);
+
+    /* Recalculate simplebar on sidebar dropdown toggle */
+    const sidebarDropdowns = document.querySelectorAll(".js-sidebar [data-bs-parent]");
+    
+    sidebarDropdowns.forEach(link => {
+      link.addEventListener("shown.bs.collapse", () => {
+        simplebarInstance.recalculate();
+      });
+      link.addEventListener("hidden.bs.collapse", () => {
+        simplebarInstance.recalculate();
+      });
     });
-    link.addEventListener("hidden.bs.collapse", () => {
-      simplebarInstance.recalculate();
-    });
-  });
+  }
 }
 
 const initializeSidebarCollapse = () => {
   const sidebarElement = document.getElementsByClassName("js-sidebar")[0];
   const sidebarToggleElement = document.getElementsByClassName("js-sidebar-toggle")[0];
 
-  sidebarToggleElement.addEventListener("click", () => {
-    sidebarElement.classList.toggle("collapsed");
+  if(sidebarElement && sidebarToggleElement) {
+    sidebarToggleElement.addEventListener("click", () => {
+      sidebarElement.classList.toggle("collapsed");
 
-    sidebarElement.addEventListener("transitionend", () => {
-      window.dispatchEvent(new Event("resize"));
+      sidebarElement.addEventListener("transitionend", () => {
+        window.dispatchEvent(new Event("resize"));
+      });
     });
-  });
+  }
 }
 
 // Wait until page is loaded
