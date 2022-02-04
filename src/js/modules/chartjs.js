9ddc8869e26e39d0aa43a336b8a2e4c25240260f
Paul Laros <paul@laros.io>
2022-02-04 21:44:45 +0100

    Set chart.js defaulFontColor

--- src/js/modules/chartjs.js
+++ src/js/modules/chartjs.js
@@ -1,5 +1,6 @@
 // Usage: https://www.chartjs.org/
 import Chart from "chart.js";
 
+Chart.defaults.global.defaultFontColor = window.theme["gray-600"];
 Chart.defaults.global.defaultFontFamily = "'Inter', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
 window.Chart = Chart;
