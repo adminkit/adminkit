de64adc1cea7cccbf7129c247d9195b67bdb3dd8
Paul Laros <paul@laros.io>
2023-06-08 22:49:40 +0200

    Add error catching to feather icons

--- src/js/modules/feather.js
+++ src/js/modules/feather.js
@@ -2,7 +2,13 @@
 import feather from "feather-icons";
 
 document.addEventListener("DOMContentLoaded", () => {
+  try {
     feather.replace();
+  }
+  catch(e) {
+    console.log('You might have made a typo with one of the feather icons');
+    console.log(e);
+  }
 });
 
 window.feather = feather;
